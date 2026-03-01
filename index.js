25d9eb58047897cf1b6b5116442f8365d802f6d7
web <jamesphansen@proton.me>
2026-02-28 23:53:04 -0800

    Recieve json payload

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const PORT = 3000;
 
 app.use(express.json())
 
-/*
 app.post('/start-monitoring', (req, res) => {
+	const { symbol, minutes, seconds } = req.body
+	if (!symbol)
+		res.status(401).send( { message: 'symbol must be a nonempty string' } )
+	if (minutes < 0 || seconds < 0)
+		res.status(402).send( { message: 'time cannot be negative' } )
 });
 
 app.get('/history?symbol=<stockSymbol>', (req, res) => {
@@ -13,7 +17,6 @@ app.get('/history?symbol=<stockSymbol>', (req, res) => {
 
 app.post('/refresh', (req, res) => {
 });
-*/
 
 app.listen(
 	PORT,
